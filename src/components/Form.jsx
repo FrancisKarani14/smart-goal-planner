6b14182101d01d779fbcf5daafab0087155cb939
Francis <franciskarani15@gmail.com>
2025-07-20 11:15:54 +0300

    styles the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../reset.css'
 
 export default function Form() {
   return (
-    <form>
+    <form >
       <input type='text' name='' placeholder='Add your savings goal' />
        <input type='number' name='' placeholder='your Target Amount' />
         <input type='number' name='' placeholder='Add your saved amount' />
@@ -10,6 +11,7 @@ export default function Form() {
          <input type='text' name='' placeholder='enter your category'/>
          <input type='date' name='' placeholder='Add your Deadline'/>
          <input type='date' name='' placeholder='enter current date'/>
+         <button>Add Your Goal</button>
          
          
         
