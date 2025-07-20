a215bd264bea1fe7cddbe89bf696dd271a6c2ec2
Francis <franciskarani15@gmail.com>
2025-07-20 16:15:26 +0300

    changes the location of db.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import Display from './Display'
+import Goalscard from './Goalscard'
 import Form from './Form'
 import '../reset.css'
 
 function App() {
+// Fetch request as a sude effect since it fetches after render.
+  useEffect(()=>{
+    fetch(" http://localhost:3000/goals")
+
+  },[])
+
   return (
     <div className='body'>
       <h1 className='welcome'>Welcome to Smart Goal Planner</h1>
       <Form />
-      <Display />
+      <div>
+        <ul>
+          <Goalscard /> 
+        </ul>
+
+      </div>
     </div>
   )
 }
--- src/components/Goalscard.jsx
+++ src/components/Goalscard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Display() {
+export default function Goalscard() {
   return (
     <div>
       
