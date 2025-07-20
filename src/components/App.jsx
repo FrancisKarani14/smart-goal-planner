e27f745f9cd796ec8606a06ae46bc9af71ff8b29
Francis <franciskarani15@gmail.com>
2025-07-20 08:58:10 +0300

    adds json servers db.json

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Display from './Display'
+import Form from './Form'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Smart Goal Planner</h1>
+      <Form />
+      <Display />
+    </>
+  )
+}
+
+export default App
