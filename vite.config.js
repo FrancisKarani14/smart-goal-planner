21f1667982fb1d87bf047177dd5f95cc08ddaf45
Francis <franciskarani15@gmail.com>
2025-07-25 15:01:35 +0300

    uninstalls gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/FrancisKarani14/smart-goal-planner",
   plugins: [react()],
 })
